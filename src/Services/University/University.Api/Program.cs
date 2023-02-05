457570a4858ce2fbcc0b11ffa1df132194b4bb5c
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-05 09:18:05 +0330

    Change logger to serilog

--- src/Services/University/University.Api/Program.cs
+++ src/Services/University/University.Api/Program.cs
@@ -1,7 +1,11 @@
+using Serilog;
 using University.Application;
 using University.Infrasturcture;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders()
+               .AddSerilog();
 
 builder.Services.RegistrarApplicationServices()
                 .RegistrarInfrastructureServices(builder.Configuration);
@@ -11,6 +15,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(app.Configuration)
+    .CreateLogger();
 
 app.UseSwagger();
 app.UseSwaggerUI();
