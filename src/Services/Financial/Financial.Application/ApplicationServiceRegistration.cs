3273c83638971266a371251f3b36c72214aa0cc3
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-07 16:46:02 +0330

    Add ValidationBehaviour and AutoMapper

--- src/Services/Financial/Financial.Application/ApplicationServiceRegistration.cs
+++ src/Services/Financial/Financial.Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Financial.Application.Behaviours;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,9 +10,12 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection RegistrarApplicationServices(this IServiceCollection services)
     {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly(), ServiceLifetime.Scoped, null, true);
 
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
         return services;
     }
---
+++ src/Services/Financial/Financial.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using Financial.Application.Exceptions;
+using FluentValidation;
+using MediatR;
+
+namespace Financial.Application.Behaviours;
+
+internal class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validaitonResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var failures = validaitonResults.SelectMany(vr => vr.Errors).Where(f => f is not null).ToList();
+
+            if (failures.Any())
+                throw new EntityValidationException(failures);
+        }
+
+        return await next();
+    }
+}
