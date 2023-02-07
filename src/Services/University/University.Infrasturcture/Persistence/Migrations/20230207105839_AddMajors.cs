a44e4a17282430f821e93c129b20d17542ed388e
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-07 14:33:04 +0330

    Add major migration

---
+++ src/Services/University/University.Infrasturcture/Persistence/Migrations/20230207105839_AddMajors.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University.Infrasturcture.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMajors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MajorId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Majors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Majors", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_MajorId",
+                table: "Students",
+                column: "MajorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Majors_MajorId",
+                table: "Students",
+                column: "MajorId",
+                principalTable: "Majors",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Majors_MajorId",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Majors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_MajorId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "MajorId",
+                table: "Students");
+        }
+    }
+}
