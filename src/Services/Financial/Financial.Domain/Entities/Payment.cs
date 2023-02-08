b5749d79848028852d66a3d1131773497c3e7acf
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-08 09:46:50 +0330

    Fix Payment entity

--- src/Services/Financial/Financial.Domain/Entities/Payment.cs
+++ src/Services/Financial/Financial.Domain/Entities/Payment.cs
@@ -4,7 +4,7 @@ namespace Financial.Domain.Entities;
 
 public class Payment : EntityBase
 {
-    public int StudentId { get; set; }
+    public string StudentNumber { get; set; }
     public decimal Amount { get; set; }
     public DateTime PaymentTime { get; set; }
     public bool IsSuccess { get; set; }
