77c063e079c0fd93e677394e66d0a56d746cb3e2
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-08 08:16:20 +0330

    Add Debt entity missing properties

--- src/Services/Financial/Financial.Domain/Entities/Debt.cs
+++ src/Services/Financial/Financial.Domain/Entities/Debt.cs
@@ -5,7 +5,9 @@ namespace Financial.Domain.Entities;
 
 public class Debt : EntityBase
 {
+    public string StudentNumber { get; set; }
     public string SourceId { get; set; }
+    public decimal Amount { get; set; }
     public DebtSourseType DebtSourseType { get; set; }
     public DateTime DateTime { get; set; }
     public bool IsDeleted { get; set; }
