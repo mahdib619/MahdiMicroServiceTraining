6bdbe9c03ca6315bc363a99367e5b3328cf35cb3
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-11 11:53:10 +0330

    Add CourseFee controller

---
+++ src/Services/Financial/Financial.Api/Controllers/CourseFeesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Financial.Application.Dtos.CourseFee;
+using Financial.Application.Features.CourseFees.Command.AddOrUpdateCourseFee;
+using Financial.Application.Features.CourseFees.Queries.GetCourseFeeByCourseId;
+using Financial.Application.Features.CourseFees.Queries.GetCourseFeesList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Financial.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CourseFeesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CourseFeesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<GetCourseFeeDto>>> GetAllCourseFees()
+    {
+        var courseFees = await _mediator.Send(new GetCourseFeesListQuery());
+        return Ok(courseFees);
+    }
+
+    [HttpGet("{courseId:int}", Name = nameof(GetCourseFee))]
+    public async Task<ActionResult<GetCourseFeeDto>> GetCourseFee(int courseId)
+    {
+        var request = new GetCourseFeeByCourseIdQuery { CourseId = courseId };
+        var result = await _mediator.Send(request);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GetCourseFeeDto>> AddOrUpdateCourseFee(AddOrUpdateCourseFeeCommand request)
+    {
+        var result = await _mediator.Send(request);
+        return CreatedAtAction(nameof(GetCourseFee), new { result.CourseId }, result);
+    }
+}
