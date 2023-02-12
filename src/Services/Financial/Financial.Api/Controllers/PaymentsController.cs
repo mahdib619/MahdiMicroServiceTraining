8a0acdac885df6d0a5c9f9d667513c53aa0cf916
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-12 09:08:43 +0330

    Add PaymentsController

---
+++ src/Services/Financial/Financial.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,34 @@
+﻿using Financial.Application.Dtos.Payment;
+using Financial.Application.Features.Payments.Commands.CreatePayment;
+using Financial.Application.Features.Payments.Queries.GetStudentSuccessfulPayments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Financial.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PaymentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PaymentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<GetPaymentDto>>> GetStudentPayments([FromQuery] string studentNumber)
+    {
+        var request = new GetStudentSuccessFulPaymentsQuery(studentNumber);
+        var result = await _mediator.Send(request);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GetPaymentDto>> CreatePayment(CreatePaymentCommand request)
+    {
+        var result = await _mediator.Send(request);
+        return Ok(result);
+    }
+}
