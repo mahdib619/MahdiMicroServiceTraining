7f6c0249bc2264c1a3efe0c8244f4b7914f87841
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-12 09:16:48 +0330

    Add DebtsController

---
+++ src/Services/Financial/Financial.Api/Controllers/DebtsController.cs
@@ -0,0 +1,27 @@
+﻿using Financial.Application.Dtos.Debt;
+using Financial.Application.Features.Debts.Queries.GetStudentActiveDebts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Financial.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class DebtsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DebtsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<List<GetDebtDto>>> GetStudentDebts([FromQuery] string studentNumber)
+    {
+        var request = new GetStudentActiveDebtsQuery(studentNumber);
+        var result = await _mediator.Send(request);
+        return Ok(result);
+    }
+}
