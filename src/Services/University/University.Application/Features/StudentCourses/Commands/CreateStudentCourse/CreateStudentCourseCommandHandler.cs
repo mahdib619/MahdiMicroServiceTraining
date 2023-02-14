8b2222e2f9a0d4af54872187dd3932642094268d
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-14 07:51:21 +0330

    Check StudentCourse db state before insert

--- src/Services/University/University.Application/Features/StudentCourses/Commands/CreateStudentCourse/CreateStudentCourseCommandHandler.cs
+++ src/Services/University/University.Application/Features/StudentCourses/Commands/CreateStudentCourse/CreateStudentCourseCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using GeneralHelpers.Exceptions;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using University.Application.Contracts.Persistence;
@@ -10,18 +11,28 @@ namespace University.Application.Features.StudentCourses.Commands.CreateStudentC
 internal class CreateStudentCourseCommandHandler : IRequestHandler<CreateStudentCourseCommand, GetStudentCourseDto>
 {
     private readonly IStudentCoursesRepository _repository;
+    private readonly IStudentsRepository _studentsRepository;
+    private readonly ICoursesRepository _coursesRepository;
     private readonly IMapper _mapper;
     private readonly ILogger<CreateStudentCourseCommandHandler> _logger;
 
-    public CreateStudentCourseCommandHandler(IStudentCoursesRepository repository, IMapper mapper, ILogger<CreateStudentCourseCommandHandler> logger)
+    public CreateStudentCourseCommandHandler(IStudentCoursesRepository repository,
+        IStudentsRepository studentsRepository,
+        ICoursesRepository coursesRepository,
+        IMapper mapper,
+        ILogger<CreateStudentCourseCommandHandler> logger)
     {
         _repository = repository;
+        _studentsRepository = studentsRepository;
+        _coursesRepository = coursesRepository;
         _mapper = mapper;
         _logger = logger;
     }
 
     public async Task<GetStudentCourseDto> Handle(CreateStudentCourseCommand request, CancellationToken cancellationToken)
     {
+        await CheckStudentCoursePersistedState(request);
+
         var studentCourse = _mapper.Map<StudentCourse>(request);
         var addedStudentCourse = await _repository.AddAsync(studentCourse);
 
@@ -30,5 +41,35 @@ internal class CreateStudentCourseCommandHandler : IRequestHandler<CreateStudent
         _logger.LogInformation("New StudentCourse:{@StudentCourse} is added successfully.", studentCourseRes);
 
         return studentCourseRes;
+    }
+
+    private async Task CheckStudentCoursePersistedState(CreateStudentCourseCommand request)
+    {
+        await CheckCourseExistence(request);
+        await CheckStudentExistence(request);
+        await CheckStudentCourseExistence(request);
+    }
+
+    private async Task CheckStudentCourseExistence(CreateStudentCourseCommand request)
+    {
+        var exists = await _repository.AnyAsync(e => e.StudentId == request.StudentId && e.CourseId == request.CourseId &&
+                                                     (e.IsPassed || (e.TermId == request.TermId && !e.IsDeleted)));
+
+        if (exists)
+            throw new ClientException("Student has already taken this course!");
+    }
+
+    private async Task CheckStudentExistence(CreateStudentCourseCommand request)
+    {
+        var exists = await _studentsRepository.AnyAsync(e => e.Id == request.StudentId);
+        if (!exists)
+            throw new ClientException("Invalid StudentId!");
+    }
+
+    private async Task CheckCourseExistence(CreateStudentCourseCommand request)
+    {
+        var exists = await _coursesRepository.AnyAsync(e => e.Id == request.CourseId);
+        if (!exists)
+            throw new ClientException("Invalid CourseId!");
     }
 }
