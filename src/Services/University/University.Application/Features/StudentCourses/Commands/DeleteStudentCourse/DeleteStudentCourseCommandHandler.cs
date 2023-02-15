acb8c4a8665113ac061d713cdafe69e42d03b9ff
Mehdi Boghraei <mahdib619@gmail.com>
2023-02-15 08:55:48 +0330

    Implement publishing DeletedCourseEvent

--- src/Services/University/University.Application/Features/StudentCourses/Commands/DeleteStudentCourse/DeleteStudentCourseCommandHandler.cs
+++ src/Services/University/University.Application/Features/StudentCourses/Commands/DeleteStudentCourse/DeleteStudentCourseCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using GeneralHelpers.Exceptions;
+﻿using System.Linq.Expressions;
+using EventBus.Messages.Events;
+using GeneralHelpers.Exceptions;
+using MassTransit;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using University.Application.Contracts.Persistence;
@@ -9,38 +12,57 @@ namespace University.Application.Features.StudentCourses.Commands.DeleteStudentC
 internal class DeleteStudentCourseCommandHandler : IRequestHandler<DeleteStudentCourseCommand>
 {
     private readonly IStudentCoursesRepository _repository;
+    private readonly IPublishEndpoint _publishEndpoint;
     private readonly ILogger<DeleteStudentCourseCommandHandler> _logger;
 
-    public DeleteStudentCourseCommandHandler(IStudentCoursesRepository repository, ILogger<DeleteStudentCourseCommandHandler> logger)
+    public DeleteStudentCourseCommandHandler(IStudentCoursesRepository repository, IPublishEndpoint publishEndpoint, ILogger<DeleteStudentCourseCommandHandler> logger)
     {
         _repository = repository;
+        _publishEndpoint = publishEndpoint;
         _logger = logger;
     }
 
     public async Task<Unit> Handle(DeleteStudentCourseCommand request, CancellationToken cancellationToken)
     {
-        var studentCourse = await _repository.GetByIdAsync(request.StudentCourseId);
+        var studentCourse = await GetStudentCourse(request);
 
-        CheckStudentCourseExistence(studentCourse, request);
         CheckPassedState(studentCourse);
 
         studentCourse.IsDeleted = true;
         await _repository.UpdateAsync(studentCourse);
+
+        await PublishDeletedCourseEvent(request, studentCourse);
 
         _logger.LogInformation("StudentCourse {StudentCourseId} is deleted successfully.", request.StudentCourseId);
 
         return Unit.Value;
     }
 
-    private static void CheckStudentCourseExistence(StudentCourse studentCourse, DeleteStudentCourseCommand request)
+    private async Task<StudentCourse> GetStudentCourse(DeleteStudentCourseCommand request)
     {
-        if (studentCourse is null)
+        var studentCourseCol = await _repository.GetAsync(e => e.Id == request.StudentCourseId, null, new List<Expression<Func<StudentCourse, object>>> { e => e.Student.Major });
+
+        if (studentCourseCol.Count == 0)
             throw new NotFoundException(nameof(StudentCourse), request.StudentCourseId);
+
+        return studentCourseCol[0];
     }
 
     private static void CheckPassedState(StudentCourse studentCourse)
     {
         if (studentCourse.IsPassed)
             throw new ClientException("Cannot delete passed course!");
+    }
+
+    private async Task PublishDeletedCourseEvent(DeleteStudentCourseCommand request, StudentCourse studentCourse)
+    {
+        var message = new StudentDeletedCourseEvent
+        {
+            StudentNumber = studentCourse.Student.StudentNumber,
+            MajorCode = studentCourse.Student.Major.Code,
+            DeletedCourseId = request.StudentCourseId
+        };
+
+        await _publishEndpoint.Publish(message);
     }
 }
